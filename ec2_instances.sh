10f8a7f48f388b92cb828f9d8a4d7e0e24399aa2
srinivas gonepudi <srigonepudi@gmail.com>
2024-03-15 20:07:56 +0530

    added new roles

--- ec2_instances.sh
+++ ec2_instances.sh
@@ -2,7 +2,7 @@
 
 AMI_ID="ami-0f3c7d07486cad139"
 SG_ID="sg-0eab7d3878626d44d"
-INSTANCE_TYPE="t2.micro"
+SMALL_INSTANCE_TYPE="t2.micro"
 BIG_INSTANCES="t3.small"
 INSTANCE_NAMES=("mysql" "web" "shipping" "payment" "redis" "catalogue" "rabbit" "user" "cart" "dispatch" "mongo")
 
@@ -10,19 +10,17 @@ for name in "${INSTANCE_NAMES[@]}"
 do
     if [[ $name == "mysql" || $name == "redis" || $name == "mongo" ]]; then
         echo "Launching $name instance with $BIG_INSTANCES"
-        aws ec2 run-instances \
-        --image-id "$AMI_ID" \
-        --instance-type "$BIG_INSTANCES" \
-        --security-group-ids "$SG_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$name}]"
-        # Add your instance launching command here using $BIG_INSTANCES
+        INSTANCE_TYPE=$BIG_INSTANCES
+
     else
-        echo "Launching $name instance with $INSTANCE_TYPE"
-        aws ec2 run-instances \
-        --image-id "$AMI_ID" \
-        --instance-type "$INSTANCE_TYPE" \
-        --security-group-ids "$SG_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$name}]"
-        # Add your instance launching command here using $INSTANCE_TYPE
+        echo "Launching $name instance with $SMALL_INSTANCE_TYPE"
+        INSTANCE_TYPE=$SMALL_INSTANCE_TYPE
     fi
+    result=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type "$INSTANCE_TYPE" \
+    --security-group-ids "$SG_ID" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$name}]" \
+    --query 'Instances[0].PrivateIpAddress')
+    # Add your instance launching command here using $BIG_INSTANCES    
 done
