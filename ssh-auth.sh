9eb387fd8754bf2009735f719c755235fcd7ef88
srinivas gonepudi <srigonepudi@gmail.com>
2024-03-16 14:57:58 +0530

    added new roles

--- ssh-auth.sh
+++ ssh-auth.sh
@@ -4,6 +4,7 @@ ID=$(id -u)
 USER_DIR="/home/centos"
 SSH_DIR="$USER_DIR/.ssh"
 KEY_DIR="$SSH_DIR/rsa_key"
+FILE_FIND=$(find /home/centos/ 'rsa_key')
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -19,11 +20,12 @@ then
     echo "YOUR NOT A ROOT USER"
     exit 1
 fi   
+find "$USER_DIR" 'rsa_key'
 
-if [ ! -d $SSH_DIR ]
+if [ $? -ne 0 ]
 then
     echo "KEY ALREADY EXISTIED  So SKIPPING TO KEY CREATION"
 else
-   ssh-keygen -t rsa -f /home/centos/rsa_key 
+   ssh-keygen -t rsa -f "$USER_DIR"
    VALIDATE $? "SSH-KEY-GENERATION"
 fi
